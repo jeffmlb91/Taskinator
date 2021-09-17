805b1af6b6cf6c8d1c164c8c7f05b603889f1de7
jeffmlb91 <jeffmlb91@gmail.com>
2021-09-17 00:00:08 -0400

    added event listener

---
+++ assets/JS/script.js
@@ -0,0 +1,16 @@
+// create variable to target the button element and the ul element
+var buttonEL = document.querySelector("#save-task");
+console.log(buttonEL);
+var taskToDoEl = document.querySelector("#tasks-to-do");
+
+// EVENT HANDLER
+
+var createTaskHandler = function () {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task";
+  taskToDoEl.appendChild(listItemEl);
+};
+
+// Create a new task, style the new task, add text and append this element to the task list. EVENT LISTENER
+buttonEL.addEventListener("click", createTaskHandler);
